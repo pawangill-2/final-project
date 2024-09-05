47a59a4bb9738b2a77468782393474348f26b896
Pawan Gill <pkgill@emory.edu>
2024-09-04 21:10:52 -0400

    commit 3

--- Coffee Survey Code.R
+++ Coffee Survey Code.R
@@ -10,18 +10,17 @@ table(coffee_survey$expertise)
 table(coffee_survey$gender)
 table(coffee_survey$gender_specify)
 tbl_summary(
-	coffee_survey,
-	by = age,
-	include = c(cups, favorite, style, strength, roast_level, caffeine))
+	coffee_survey1,
+	by = cups_bin,
+	include = c(strength, roast_level, caffeine)
+	)
+
 
 tbl_summary(
-	coffee_survey,
-	by = age,
-	include = c(cups, favorite, style, strength, roast_level, caffeine),
+	coffee_survey1,
+	by = cups_bin,
+	include = c(strength, roast_level, caffeine),
 	label = list(
-		cups ~ "Cups of coffee per day",
-		favorite ~ "Favorite coffee drink",
-		style ~ "Preferred coffee style",
 		strength ~ "Preferred coffee strength",
 		roast_level ~ "Preferred coffee roast level",
 		caffeine ~ "Preffered caffeine amount"
@@ -88,7 +87,7 @@ tbl_regression(
 )
 
 
-
+saveRDS(coffee_survey, "/Users/pawanpreetgill/Desktop/R Bootcamp/final-project/coffee_survey1.rds")
 
 
 
